2ea3292c5d640e22e66bbcbe3f5b4eb3c4232e01
ahlam touzalti <ahlamtouzalti@gmail.com>
2023-04-25 13:11:33 -0700

    alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,19 @@
-#include<main.h>
- /**
- * print_alphabet_x10-prints lowercase alphabets 10 times 
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets 10 times 
  */
- void print_alphabet_x10(void)
-{   
+void print_alphabet_x10(void)
+{
     int ten;
     char la;
 
-    for ( 10 = 0 ; 10 <= 9 ; ten++) 
-      { for (la ='a' ; la <= 'z' ;la++) 
-           _putchar (la) ;
-        _putchar ('/n');
-      } 
- } 
+    for (ten = 0; ten < 10; ten++)
+    {
+        for (la = 'a'; la <= 'z'; la++)
+        {
+            _putchar(la);
+        }
+        _putchar('\n');
+    }
+}
